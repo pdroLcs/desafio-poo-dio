22c66eecaeaa33ee6d74f7caa69c57b0abe49d38
Pedro Lucas <plfm3k@gmail.com>
2025-10-20 10:59:33 -0400

    Adicionado classes Mentoria e Curso

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de Java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1.toString());
+        System.out.println(mentoria.toString());
     }
 }
